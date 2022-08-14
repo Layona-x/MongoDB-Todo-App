8e703a3551cf04e5c0481b3981377423735ace6c
Layona-x <109739779+Layona-x@users.noreply.github.com>
2022-08-14 10:15:15 +0300

    Add files via upload

--- models/todos.js
+++ models/todos.js
@@ -1,1 +1,20 @@
+const mongoose = require('mongoose')
+let Schema = mongoose.Schema
 
+const todoSchema = new Schema({
+  title:{
+    type:String,
+    require:true,
+  },
+  description:{
+    type:String,
+    require: true
+  },
+  status:{
+    type:String,
+    require:true
+  }
+})
+
+let todo = mongoose.model("Todo", todoSchema)
+module.exports = todo;
